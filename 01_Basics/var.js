e4bb4f058f3f64c6c69d388b896080a421c65262
parastrivedi22 <140546779+parastrivedi22@users.noreply.github.com>
2024-03-14 00:25:58 +0530

    variable basic

---
+++ 01_Basics/var.js
@@ -0,0 +1,8 @@
+console.log("Hello world");
+
+const userId = 132322; // const does not allow to change the value after initialization
+let userName = "Ram Prashad"; // let solve the scope proble
+var userPassword = "paras#R@#"; // it has scope problem
+isEligible = false; // you can write like this but this is bad practis.
+
+console.log(isEligible);
