9cf13d32eb9e2767ea894bc8dd4ae829733ff156
parastrivedi22 <140546779+parastrivedi22@users.noreply.github.com>
2024-03-14 23:56:00 +0530

    Type conversion operation
    I performed many operation to hands on type conversion in javascript.

---
+++ 01_Basics/01_conversion.js
@@ -0,0 +1,24 @@
+let someNumber = 231;
+
+// console.log(typeof someNumber);
+// console.log(someNumber);
+
+// type conversion
+
+let isString = String(someNumber);
+// console.log(isString);
+// console.log(typeof isString);
+
+let something = undefined;
+
+let change = Number(something);
+
+console.log(something);
+console.log(change);
+
+// "akda" => NaN
+// "" => 0
+// "a" => NaN
+// "234" => 234
+// null => 0
+// undefined => NaN
