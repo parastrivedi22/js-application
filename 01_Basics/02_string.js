e38f50512be8b27f541ee16af210a3217bfe48ec
parastrivedi22 <140546779+parastrivedi22@users.noreply.github.com>
2024-03-16 20:18:31 +0530

    play with string and its methods.

---
+++ 01_Basics/02_string.js
@@ -0,0 +1,14 @@
+let str = "hello world";
+
+let str1 = new String("hello world");
+let str2 = str1;
+str2 = "paras";
+
+// console.log(str2);
+// console.log(str1);
+
+// console.log(str.endsWith("0ld"));
+// console.log(str.charCodeAt());
+// console.log(str2.indexOf("r"));
+// console.log(str2.slice(0, 2));
+// console.log(str1 == str);
