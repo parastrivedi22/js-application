6df29f6253c3f01e1aa5b1603bae5f1e52c94f5e
parastrivedi22 <140546779+parastrivedi22@users.noreply.github.com>
2024-03-16 19:18:23 +0530

    data types of js

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,10 @@
+
+
+// primite type and refference type
+//  +share the copy 
+Number, String, null, undefined, Boolean,  Symbol,
+
+// non-primitive
+// + share the memory refference of the variable
+Array, Function, object,
+
