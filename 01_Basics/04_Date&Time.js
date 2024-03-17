bbbef6bffe3ea79114f76f0664dfb37164c027c5
parastrivedi22 <140546779+parastrivedi22@users.noreply.github.com>
2024-03-17 22:15:25 +0530

    hands on with date and times

---
+++ 01_Basics/04_Date&Time.js
@@ -0,0 +1,28 @@
+let date = new Date();
+
+// console.log(date);
+// console.log(date.toISOString());
+// console.log(date.getTime());
+// console.log(date.getFullYear());
+// console.log(date.getDate());
+// console.log(date.getMilliseconds());
+// console.log(date.getTimezoneOffset());
+
+// console.log(date.toTimeString());
+// console.log(date.toString());
+// console.log(date.toJSON());
+
+// console.log(date.toDateString()); // Sun Mar 17 2024
+// console.log(date.toLocaleDateString()); //17/3/2024
+// console.log(date.toLocaleString()); //17/3/2024, 9:56:07 pm
+
+// let myCreatedDate = new Date(2024, 0, 32);
+// let myCreatedDate = new Date(2024, 0, 32, 5, 30);
+// let myCreatedDate = new Date("2024-2-23");
+// console.log(myCreatedDate.toString());
+
+// console.log(Date.now()); // it will give you time in milisencond
+
+// console.log(`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`); //17/3/2024
+
+// console.log(date.toLocaleString("default", { weekday: "long", date: "date" }));
