c9ffcbf7014e255ded509928eaefa0efb5acceb1
parastrivedi22 <140546779+parastrivedi22@users.noreply.github.com>
2024-03-28 23:53:46 +0530

    got the basic understanding of the objects

--- 02_Basics/03_object.js
+++ 02_Basics/03_object.js
@@ -14,7 +14,22 @@ let JsUser = {
 
 // we can access object's value in two way
 
-console.log(JsUser);
-console.log(JsUser.name); // by dot operator
-console.log(JsUser["name"]); // by square bracket notation
-console.log(JsUser["pass out"]); // we can not use coqurt key throgh dot operator
+// console.log(JsUser);
+// console.log(JsUser.name); // by dot operator
+// console.log(JsUser["name"]); // by square bracket notation
+// console.log(JsUser["pass out"]); // we can not use coqurt key throgh dot operator
+
+// store symbol in the object
+
+let myAge = Symbol(23);
+
+let JsUser2 = {
+  name: "Paras",
+  [myAge]: "22", // need to use square notation to use Symbol as a key
+  location: "Pune",
+  "pass out": 2021, // key should be without space, other wise write inside the coqurt
+};
+
+console.log(JsUser2);
+console.log(JsUser2.myAge); // can not access Symbol key throgh dot
+console.log(JsUser2[myAge]);
