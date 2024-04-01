4ead124a7839b07cbf6101d7fe64c924111577ab
parastrivedi22 <140546779+parastrivedi22@users.noreply.github.com>
2024-04-01 12:53:17 +0530

    javascript falsy and truthy values

--- 03_Basics/02,iife.js
+++ 03_Basics/02,iife.js
@@ -12,3 +12,22 @@
 ((city) => {
   console.log(`Welcome to ${city}`);
 })("London");
+
+// Falsy value in JavaScript
+// false, 0, -0, Bigint 0n, "", null, undefined NaN
+
+// truthy value,
+//  true, " ", "false", [], {}, functions(){},
+
+//we can not compare [], {}
+// if(arr.length === 0)
+// if(Object.keys(obj).length === 0)
+
+// Nullish coalescing Operator ?? : null , undefined
+
+let a = [232, 23, 23];
+// let a = undefined;
+
+let b = a ?? 123; // it provides to push null or undefined value into the variable
+
+console.log(b);
